25e129a77853b4bd3d479c86719029e5fd37146a
pabloacalvo@live.com <pabloacalvo@live.com>
2025-07-09 00:36:01 -0300

    Clase 10 terminada

--- productos/urls.py
+++ productos/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns  = [
     path('', views.index, name='index'),
-    path('<int:product_id>/detail', views.detail, name='detail')
+    path('<int:product_id>/detail', views.detail, name='detail'),
+    path('category/<int:category_id>', views.get_products_by_category, name='category'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -2,7 +2,8 @@ from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
-from productos.models import Product
+from productos.models import Product, Category
+
 
 # Create your views here.
 def index(request):
@@ -21,3 +22,14 @@ def detail(request, product_id):
     }
 
     return render(request, 'products/detail.html', context)
+
+def get_products_by_category(request,category_id:int):
+    category = Category.objects.prefetch_related('products').get(id=category_id)
+    products = category.products.all()
+
+    context = {
+        "products":products,
+        "category":category
+    }
+
+    return render(request, "products/category.html", context)
